53c41e6cdb14fc242d531967a1196ae8e3f16392
Sharon Lin <sharonlin14@gmail.com>
2017-01-17 10:50:41 -0500

    Added small update

---
+++ server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
