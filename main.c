c343ecabd08cec5a974b1ee8b937d5f71fe70d2f
Sharon Lin <sharonlin14@gmail.com>
2017-01-17 10:56:36 -0500

    added networking files

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include "print_ascii.c"
+#include "server.c"
+#include "client.c"
