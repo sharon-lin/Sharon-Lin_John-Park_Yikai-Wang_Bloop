115138f0f063d7647ff2e81277b61836807001df
getDolla <ywang8@stuy.edu>
2017-01-17 22:53:47 -0500

    tryna figure out how to do ascii in a row using loops

--- print_ascii.c
+++ print_ascii.c
@@ -1,5 +1,7 @@
-#define ASCII_SIZE1 1420
-//#define ASCII_SIZE2 1949
+#define ASCII_SIZE1 70
+#define ASCII_SIZE2 72
+#define ASCII_SIZE3 80
+#define ASCII_SIZE4 92
 
 #include <sys/sem.h>
 #include <stdio.h>
@@ -13,20 +15,32 @@
 #include <errno.h>
 #include <unistd.h>
 
-void main(){
+void main(//int argc, char const *argv[]
+) {
   int fd = open("ascii.txt", O_RDONLY);
 
-  char buff[ASCII_SIZE1];
-  read( fd, buff, sizeof(buff) );
-  buff[ASCII_SIZE1] = 0;
-  printf( "%s\n", buff );
+  char buff[4][100];
+  read( fd, buff[0], ASCII_SIZE1 );
+  buff[0][ASCII_SIZE1] = 0;
+  printf( "buff[0]:\n%s\n", buff[0] );
 
-  //gets # of bytes the current position is from the beginning of the file
-  //lseek(fd, -(ASCII_SIZE2), SEEK_END );//from end
+  lseek(fd, 1, SEEK_CUR );
 
-  //read( fd, buff, sizeof(buff) );
-  //buff[ASCII_SIZE2] = 0;
-  //printf( "%s\n", buff );
+  read( fd, buff[1], ASCII_SIZE2 );
+  buff[1][ASCII_SIZE2] = 0;
+  printf( "buff[1]:\n%s\n", buff[1] );
+
+  lseek(fd, 1, SEEK_CUR );
+
+  read( fd, buff[2], ASCII_SIZE3 );
+  buff[2][ASCII_SIZE3] = 0;
+  printf( "buff[2]:\n%s\n", buff[2] );
+
+  lseek(fd, 1, SEEK_CUR );
+
+  read( fd, buff[3], ASCII_SIZE4 );
+  buff[3][ASCII_SIZE4] = 0;
+  printf( "buff[3]:\n%s\n", buff[3] );
 
   close(fd);
 }
