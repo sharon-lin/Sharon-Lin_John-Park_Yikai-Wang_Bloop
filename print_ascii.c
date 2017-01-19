c499df940868766a62a197f1d383adc8b04197f7
Yikai <ywang8@stuy.edu>
2017-01-19 12:14:01 -0500

    working on ascii

--- print_ascii.c
+++ print_ascii.c
@@ -1,5 +1,5 @@
-#define ASCII_SIZE 15
-#define MAX_ASCII_SIZE 200
+#define ASCII_SIZE 16
+#define MAX_ASCII_SIZE 250
 
 #include <sys/sem.h>
 #include <stdio.h>
@@ -14,7 +14,7 @@
 #include <unistd.h>
 
 int ascii_sizes[ASCII_SIZE] = {
-  183, 186, 119,
+  183, 186, 119, 186,
   7, 10, 13,
   8, 9, 14,
   1, 10, 11,
@@ -31,12 +31,45 @@ void read_all( char buff[ASCII_SIZE][MAX_ASCII_SIZE], int fd ) {
   }
 }
 
-void main(//int argc, char const *argv[]
+void print_drums( char buff[ASCII_SIZE][MAX_ASCII_SIZE], char *key ) {
+  int i = atoi(key);
+  if( i%3 == 1 ) {
+    char * s = "                  ";
+    printf("%s%s\n", s, buff[11] );
+    printf("%s%s\n", s, buff[12] );
+    printf("%s\n", buff[2] );
+  }
+  if( i%3 == 2 ) {
+    printf("%s\n", buff[11] );
+    printf( "%s \n", buff[12] );
+    printf("%s\n", buff[3] );
+  }
+}
+
+void print_hit( char buff[ASCII_SIZE][MAX_ASCII_SIZE], char *key ) {
+    char c = key[0];
+    int a = 0;
+
+    if( (c > 64) && (c < 73) )
+      a = c - 64;
+    else if( (c > 96) && (c < 105) )
+      a = c - 96;
+
+    int i = 0;
+    for(; i < a; i++) {
+
+    }
+}
+
+void main(int argc, char const *argv[]
 ) {
   int fd = open("ascii.txt", O_RDONLY);
 
   char buff[ASCII_SIZE][MAX_ASCII_SIZE];
   read_all( buff, fd );
-
   close(fd);
+
+  printf("argv[2]: %s\n", argv[2] );
+  print_drums( buff, argv[2]);
+
 }
