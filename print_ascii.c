22e9bf3bc328d180b024355bf9b9a99d6f3fe473
getDolla <ywang8@stuy.edu>
2017-01-21 17:37:42 -0500

    finished set_up drums

--- print_ascii.c
+++ print_ascii.c
@@ -32,34 +32,57 @@ void read_all( char buff[ASCII_SIZE][MAX_ASCII_SIZE], int fd ) {
   }
 }
 
+// void print_top( char buff[ASCII_SIZE][MAX_ASCII_SIZE], int i, char isHit ) {
+//   if( i > 2 ) {
+//     if()
+//     char * s1 = "      ";
+//     char * s2 = "     ";
+//     char * s3 = "                         ";
+// }
+
 void print_drums( char buff[ASCII_SIZE][MAX_ASCII_SIZE], char *key ) {
   int i = atoi(key);
+  char c = 65; char diff = 0;
+
+  if( i > 2 ) {
+    char * s1 = "      ";
+    char * s2 = "     ";
+    char * s3 = "                         ";
+    printf("%s%s%s\n", buff[11], s3, buff[11]);
+    printf("%s%s%s%s%s\n", buff[12], s1, buff[15], s2, buff[12] );
+    printf("%s\n", buff[0] );
+    printf("     %c    %s       %c      %s    %c\n", c, s1, c + 1, s2, c + 2 );
+    diff += 3;
+
+    if( i > 5 ) {
+      s1 = "     ";
+      s2 = "    ";
+      s3 = "                  ";
+      printf("%s%s\n", s3, buff[11] );
+      printf("%s%s%s%s%s\n", buff[15], s1, buff[12], s2, buff[15] );
+      printf("%s\n", buff[1] );
+      printf("       %c      %s    %c     %s      %c\n", c + diff, s1, c + diff + 1, s2, c + diff + 2 );
+      diff += 3;
+    }
+  }
+
   if( i%3 == 1 ) {
     char * s = "                  ";
     printf("%s%s\n", s, buff[11] );
     printf("%s%s\n", s, buff[12] );
     printf("%s\n", buff[2] );
+    printf("%s     %c\n", s, c + diff );
   }
   if( i%3 == 2 ) {
+    char * s = "                      ";
     printf("%s\n", buff[11] );
-    printf( "%s                      %s\n", buff[12], buff[15] );
+    printf( "%s%s%s\n", buff[12], s, buff[15] );
     printf("%s\n", buff[3] );
+    printf("     %c    %s       %c\n", c + diff, s, c + diff + 1 );
   }
 }
 
 void print_hit( char buff[ASCII_SIZE][MAX_ASCII_SIZE], char *key ) {
-    char c = key[0];
-    int a = 0;
-
-    if( (c > 64) && (c < 73) )
-      a = c - 64;
-    else if( (c > 96) && (c < 105) )
-      a = c - 96;
-
-    int i = 0;
-    for(; i < a; i++) {
-
-    }
 }
 
 void main(int argc, char const *argv[]
@@ -70,7 +93,7 @@ void main(int argc, char const *argv[]
   read_all( buff, fd );
   close(fd);
 
-  printf("argv[2]: %s\n", argv[2] );
-  print_drums( buff, argv[2]);
+  printf("argv[1]: %s\n", argv[1] );
+  print_drums( buff, argv[1]);
 
 }
