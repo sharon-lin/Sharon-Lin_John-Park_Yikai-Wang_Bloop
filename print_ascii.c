a5fcf18399e2bd8eade48c791991597880f9ad47
getDolla <ywang8@stuy.edu>
2017-01-21 22:56:19 -0500

    finished coding hitting drums animation

--- print_ascii.c
+++ print_ascii.c
@@ -9,6 +9,7 @@
 #include <sys/shm.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -32,49 +33,82 @@ void read_all( char buff[ASCII_SIZE][MAX_ASCII_SIZE], int fd ) {
   }
 }
 
-void print_top( char buff[ASCII_SIZE][MAX_ASCII_SIZE], int i, char * keyhit, char * s1, char * s2, char * s3 ) {
-  int isHit = atoi(keyhit);
+void print_top( char buff[ASCII_SIZE][MAX_ASCII_SIZE], int i, char keyhit, char * s1, char * s2, char * s3 ) {
+  int key = toupper(keyhit);
   if( i == 6 ) {
-    if(!isHit) {
-      printf("%s%s\n", s3, buff[11] );
-      printf("%s%s%s%s%s\n", buff[15], s1, buff[12], s2, buff[15] );
-    }
+      if((key < 'D') || (key > 'F')) {
+        printf("%s%s\n", s3, buff[11] );
+        printf("%s%s%s%s%s\n", buff[15], s1, buff[12], s2, buff[15] );
+      }
 
-    else {
-
-    }
+      else if( key == 'D' ) {
+          printf("%s\n", buff[7] );
+          printf("%s     %s%s\n", buff[8], s1, buff[11] );
+          printf("%s%s%s%s%s\n", buff[9], s1, buff[12], s2, buff[15] );
+        }
+      else if( key == 'E' ) {
+         printf("%s%s\n", s3, buff[4] );
+         printf("%s%s\n", s3, buff[5] );
+         printf("%s%s%s%s%s\n", buff[15], s1, buff[6], s2, buff[15] );
+      }
+      else if( key == 'F' ) {
+        printf("%s%s          %s\n", s3, s2, buff[7] );
+        printf("%s%s%s %s\n", s3, buff[11], s2, buff[8] );
+        printf("%s%s%s%s%s\n", buff[15], s1, buff[12], s2, buff[9] );
+      }
   }
 
   else if( i == 3 ) {
-    if(!isHit) {
-      printf("%s%s%s\n", buff[11], s3, buff[11]);
-      printf("%s%s%s%s%s\n", buff[12], s1, buff[15], s2, buff[12] );
-    }
+      if((key < 'A') || (key > 'C')) {
+        printf("%s%s%s\n", buff[11], s3, buff[11]);
+        printf("%s%s%s%s%s\n", buff[12], s1, buff[15], s2, buff[12] );
+      }
 
-    else {
-
-    }
+      else if( key == 'A' ) {
+           printf("%s\n", buff[4] );
+           printf("%s%s%s\n", buff[5], s3, buff[11] );
+           printf("%s%s%s%s%s\n", buff[6], s1, buff[15], s2, buff[12] );
+        }
+      else if( key == 'B' ) {
+        printf("          %s%s\n", s1, buff[7] );
+        printf("%s%s %s     %s%s\n", buff[11], s1, buff[8], s2, buff[11] );
+        printf("%s%s%s%s%s\n", buff[12], s1, buff[9], s2, buff[12] );
+      }
+      else if( key == 'C' ) {
+        printf("         %s%s\n", s3, buff[4] );
+        printf("%s%s%s\n", buff[11], s3, buff[5] );
+        printf("%s%s%s%s%s\n", buff[12], s1, buff[15], s2, buff[6] );
+      }
   }
 
   else if( i == 2 ) {
-    if(!isHit) {
-      printf("%s\n", buff[11] );
-      printf( "%s%s%s\n", buff[12], s1, buff[15] );
-    }
+      if((key < 'A') || (key > 'B')) {
+        printf("%s\n", buff[11] );
+        printf( "%s%s%s\n", buff[12], s1, buff[15] );
+      }
 
-    else {
-
-    }
+      else if( key == 'A' ) {
+           printf("%s\n", buff[4] );
+           printf("%s\n", buff[5] );
+           printf("%s%s%s\n", buff[6], s1, buff[15] );
+        }
+      else if( key == 'B' ) {
+        printf("          %s%s\n", s1, buff[7] );
+        printf("%s%s %s\n", buff[11], s1, buff[8] );
+        printf("%s%s%s\n", buff[12], s1, buff[9] );
+      }
   }
 
   else {
-    if(!isHit) {
+    if(key != 'A') {
       printf("%s%s\n", s1, buff[11] );
       printf("%s%s\n", s1, buff[12] );
     }
 
     else {
-
+      printf("%s%s\n", s1, buff[4] );
+      printf("%s%s\n", s1, buff[5] );
+      printf("%s%s\n", s1, buff[6] );
     }
   }
 
@@ -88,33 +122,33 @@ void print_drums( char buff[ASCII_SIZE][MAX_ASCII_SIZE], char *numkeys, char * k
     char * s1 = "      ";
     char * s2 = "     ";
     char * s3 = "                         ";
-    print_top( buff, 3, keyhit, s1, s2, s3 );
+    print_top( buff, 3, keyhit[0], s1, s2, s3 );
     printf("%s\n", buff[0] );
-    printf("     %c    %s       %c      %s    %c\n", c, s1, c + 1, s2, c + 2 );
+    printf("     %c    %s       %c      %s    %c\n\n", c, s1, c + 1, s2, c + 2 );
     diff += 3;
 
     if( i > 5 ) {
       char * s1 = "     ";
       char * s2 = "    ";
       char * s3 = "                  ";
-      print_top( buff, 6, keyhit, s1, s2, s3 );
+      print_top( buff, 6, keyhit[0], s1, s2, s3 );
       printf("%s\n", buff[1] );
-      printf("       %c      %s    %c     %s      %c\n", c + diff, s1, c + diff + 1, s2, c + diff + 2 );
+      printf("       %c      %s    %c     %s      %c\n\n", c + diff, s1, c + diff + 1, s2, c + diff + 2 );
       diff += 3;
     }
   }
 
   if( i%3 == 1 ) {
     char * s = "                  ";
-    print_top( buff, 1, keyhit, s, "", "" );
+    print_top( buff, 1, keyhit[0] + diff, s, "", "" );
     printf("%s\n", buff[2] );
-    printf("%s     %c\n", s, c + diff );
+    printf("%s     %c\n\n", s, c + diff );
   }
   if( i%3 == 2 ) {
     char * s = "                      ";
-    print_top( buff, 2, keyhit, s, "", "" );
+    print_top( buff, 2, keyhit[0] + diff, s, "", "" );
     printf("%s\n", buff[3] );
-    printf("     %c    %s       %c\n", c + diff, s, c + diff + 1 );
+    printf("     %c    %s       %c\n\n", c + diff, s, c + diff + 1 );
   }
 }
 
@@ -126,7 +160,12 @@ void main(int argc, char const *argv[]
   read_all( buff, fd );
   close(fd);
 
-  printf("argv[1]: %s\n", argv[1] );
-  print_drums( buff, argv[1], "0" );
+  printf("\nChecking for inputs (argv must be >= 2)...\n" );
+  int i = 1;
+  for(; i <= argc; i++ )
+    printf("argv[%d]: %s\n", i, argv[i] );
+
+  printf("\n" );
+  print_drums( buff, argv[1], argv[2] );
 
 }
