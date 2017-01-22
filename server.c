f52d97d5c0acdb4a9468b7dce485592989b3e441
getDolla <ywang8@stuy.edu>
2017-01-22 15:19:02 -0500

    sharon, somehow the code doesnt work on my windows 10 desktop

--- server.c
+++ server.c
@@ -9,6 +9,7 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <ctype.h>
+#include <fcntl.h>
 
 #include "networking.h"
 //#include "networking.c"
@@ -62,36 +63,49 @@ void sub_server( int sd ) {
 void play_sound(char * sequence){
 
   int length = (int)strlen(sequence);
-  int i =0;
+  int i = 0;
+
+
+  int fd = open("ascii.txt", O_RDONLY);
+  char anime[ASCII_SIZE][MAX_ASCII_SIZE];
+  read_all( anime, fd );
+  close(fd);
+
+
+  while(!isdigit(sequence[i++]));
+  int numdrums = sequence[i-1];
+  print_drums( anime, numdrums, "0" );
+
   for (;i<length;i++){
-    if (sequence[i]=='A')
+    int c = tolower(sequence[i]);
+    if ((c=='a') && (numdrums > 0)) {
+      print_drums( anime, numdrums, "a" );
       system("mpg123 sounds/sound1.mp3");
-    else if (sequence[i]=='a')
-      system("mpg123 sounds/sound1.mp3");
-    else if (sequence[i]=='B')
+    }
+    else if ((c=='b') && (numdrums > 1)) {
+      print_drums( anime, numdrums, "b" );
       system("mpg123 sounds/sound2.mp3");
-    else if (sequence[i]=='b')
-      system("mpg123 sounds/sound2.mp3");
-    else if (sequence[i]=='C')
+    }
+    else if ((c=='c') && (numdrums > 2)) {
+      print_drums( anime, numdrums, "c" );
       system("mpg123 sounds/sound3.mp3");
-    else if (sequence[i]=='c')
+    }
+    else if ((c=='d') && (numdrums > 3)) {
+      print_drums( anime, numdrums, "d" );
       system("mpg123 sounds/sound3.mp3");
-    else if (sequence[i]=='D')
-      system("mpg123 sounds/sound4.mp3");
-    else if (sequence[i]=='d')
-      system("mpg123 sounds/sound4.mp3");
-    else if (sequence[i]=='E')
+    }
+    else if ((c=='e') && (numdrums > 4)) {
+      print_drums( anime, numdrums, "e" );
       system("mpg123 sounds/sound5.mp3");
-    else if (sequence[i]=='e')
-      system("mpg123 sounds/sound5.mp3");
-    else if (sequence[i]=='F')
+    }
+    else if ((c=='f') && (numdrums > 5)) {
+      print_drums( anime, numdrums, "f" );
       system("mpg123 sounds/sound6.mp3");
-    else if (sequence[i]=='f')
-      system("mpg123 sounds/sound6.mp3");
-    else if (sequence[i]=='G')
+    }
+    else if ((c=='g') && (numdrums > 6)) {
+      print_drums( anime, numdrums, "g" );
       system("mpg123 sounds/sound7.mp3");
-    else if (sequence[i]=='g')
-    system("mpg123 sounds/sound7.mp3");
+    }
   }
 }
 
