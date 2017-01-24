447bcb558c8b902639e40cbb7a302082969208b2
Yikai <ywang8@stuy.edu>
2017-01-23 23:46:53 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,7 +1,15 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <fcntl.h>
 
 #include "networking.h"
 #include "print_ascii.h"
@@ -42,6 +50,59 @@ void play_original(char * sequence){
     }
 }
 
+void play_sound(char * sequence){
+
+  int length = (int)strlen(sequence);
+  int i = 0;
+
+  int fd = open("ascii.txt", O_RDONLY);
+  char anime[ASCII_SIZE][MAX_ASCII_SIZE];
+  read_all( anime, fd );
+  close(fd);
+
+  int numdrums; 
+
+  while(!isdigit(sequence[i++]));
+  numdrums = atoi(&sequence[i-1]);
+  
+  // printf( "%d\n", numdrums );
+  print_drums( anime, numdrums, "0" );
+
+  for (;i<length;i++) {
+    // printf( "%c\n", sequence[i] );
+    int c = tolower(sequence[i]);
+    // printf( "%c\n", sequence[i] );
+    if ((c=='a') && (numdrums > 0)) {
+      print_drums( anime, numdrums, "a" );
+      system("mpg123 sounds/sound1.mp3");
+    }
+    else if ((c=='b') && (numdrums > 1)) {
+      print_drums( anime, numdrums, "b" );
+      system("mpg123 sounds/sound2.mp3");
+    }
+    else if ((c=='c') && (numdrums > 2)) {
+      print_drums( anime, numdrums, "c" );
+      system("mpg123 sounds/sound3.mp3");
+    }
+    else if ((c=='d') && (numdrums > 3)) {
+      print_drums( anime, numdrums, "d" );
+      system("mpg123 sounds/sound3.mp3");
+    }
+    else if ((c=='e') && (numdrums > 4)) {
+      print_drums( anime, numdrums, "e" );
+      system("mpg123 sounds/sound5.mp3");
+    }
+    else if ((c=='f') && (numdrums > 5)) {
+      print_drums( anime, numdrums, "f" );
+      system("mpg123 sounds/sound6.mp3");
+    }
+    else if ((c=='g') && (numdrums > 6)) {
+      print_drums( anime, numdrums, "g" );
+      system("mpg123 sounds/sound7.mp3");
+    }
+  }
+}
+
 
 
 int main( int argc, char *argv[] ) {
@@ -67,20 +128,21 @@ int main( int argc, char *argv[] ) {
   while (1) {
     printf("Next note: ");
     fgets( buffer, sizeof(buffer), stdin );
-    char *p = strchr(buffer, '\n');
-    *p = 0;
     
-    char * cmd = strtok(buffer, " ");
-    lower_string(cmd);
-    if (strcmp(cmd,"play")==0){
-      cmd = strtok(NULL, " ");
-      play_original(cmd);
-    }
-
+    strtok(buffer, "\n");
+    printf( "received: %s\n", buffer );
+        
+      char cmd[5] = { buffer[0], buffer[1], buffer[2], buffer[3], 0 };
+      printf( "%s\n", cmd );
+      if (strcmp(cmd,"play")==0){
+        play_sound(buffer + 5);
+      }
+    
     write( sd, buffer, sizeof(buffer) );
     read( sd, buffer, sizeof(buffer) );
-    printf( "received: %s\n", buffer );
+    
     //send_message_all(buffer);
+  
   }
 
   return 0;
